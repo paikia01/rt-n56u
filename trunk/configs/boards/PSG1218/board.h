34b8cacf6b5b89dedce4fdf2cd25612811348b83
paikia01 <1226447825@qq.com>
2020-09-19 22:06:35 +0800

    Update board.h

--- trunk/configs/boards/PSG1218/board.h
+++ trunk/configs/boards/PSG1218/board.h
@@ -6,15 +6,15 @@
 #define BOARD_VENDOR_NAME	"PHICOMM"
 #define BOARD_VENDOR_URL	"http://www.phicomm.com/"
 #define BOARD_MODEL_URL		"http://www.phicomm.com/"
-#define BOARD_BOOT_TIME		25
-#define BOARD_FLASH_TIME	120
-#undef BOARD_GPIO_BTN_RESET 1
-#define BOARD_GPIO_BTN_WPS
-#undef  BOARD_GPIO_LED_ALL	 10
-#define  BOARD_GPIO_LED_WIFI
+#define BOARD_BOOT_TIME				25
+#define BOARD_FLASH_TIME			120
+#undef BOARD_GPIO_BTN_RESET
+#define BOARD_GPIO_BTN_WPS  	1
+#undef  BOARD_GPIO_LED_ALL		10
+#define  BOARD_GPIO_LED_WIFI	11
 #define BOARD_GPIO_LED_POWER	8
 #undef  BOARD_GPIO_LED_LAN
-#define  BOARD_GPIO_LED_WAN
+#define  BOARD_GPIO_LED_WAN		10
 #undef BOARD_GPIO_LED_USB 
 #undef  BOARD_GPIO_LED_ROUTER
 #undef  BOARD_GPIO_PWR_USB
